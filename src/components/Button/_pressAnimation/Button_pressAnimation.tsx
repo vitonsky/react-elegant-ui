7777b140a1129e94bcde85c888561f17fd70e36a
vitonsky <86191922+vitonsky@users.noreply.github.com>
2022-05-25 00:52:05 +0300

    feat(Button): #58 disable pressAnimation for view link

--- src/components/Button/_pressAnimation/Button_pressAnimation.tsx
+++ src/components/Button/_pressAnimation/Button_pressAnimation.tsx
@@ -1,15 +1,39 @@
-import { withClassnameHOC } from '../../../lib/compose';
+import React, { useMemo } from 'react';
 
-import { cnButton } from '../Button';
+import { withHOCConstructor } from '../../../lib/compose';
+
+import { cnButton, IButtonProps } from '../Button';
 import './Button_pressAnimation.css';
 
 export interface IModButtonPressAnimation {
 	pressAnimation?: boolean;
 }
 
-export const withModButtonPressAnimation = withClassnameHOC<IModButtonPressAnimation>(
-	cnButton(),
-	{
-		pressAnimation: true,
-	},
+export const createModButtonPressAnimation = <
+	T extends IButtonProps & Record<string, any> = any,
+>(
+		isEnabled: (props: T) => boolean,
+	) =>
+		withHOCConstructor<IModButtonPressAnimation, IButtonProps>(
+			{
+				matchProps: { pressAnimation: true },
+				matchOnlyProps: ['pressAnimation'],
+			},
+			(Component) => (props) => {
+				const pressAnimation = useMemo(
+					() => isEnabled(props as any),
+					[props],
+				);
+				return (
+					<Component
+						{...props}
+						className={cnButton({ pressAnimation }, [props.className])}
+					/>
+				);
+			},
+		);
+
+export const withModButtonPressAnimation = createModButtonPressAnimation(
+	// disable for some views
+	(props) => ['link'].indexOf(props.view) === -1,
 );
